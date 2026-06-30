eb7bfdcb98c20b258b7134a5a99c95c70f28c4bc
Iceonite <jprigby@outlook.com.au>
2026-06-30 18:29:30 +1000

    Basic framework added

---
+++ scripts/State.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+
+// Meant to define the structure of a state
+public partial class State : Node
+{
+	public StateMachine fsm; // Parent State Machine instance reference
+	public Node parent; // Overall Parent Node
+
+	public virtual void Enter() {}// When state is entered
+	public virtual void Exit() {} // When state is exited
+
+	public virtual void Ready() {} // Initilisation of the state
+	public virtual void Update(float delta) {} // Process routine
+	public virtual void PhysicsUpdate(float delta) {} // Physics routine
+	public virtual void HandleInput(InputEvent @event) {} // Input
+}
---
+++ scripts/StateMachine.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class StateMachine : Node
+{
+
+	[Export] public NodePath intialState; // Assign Intial State in the inspector
+
+	private Dictionary<string, State> _states; // Name of Node is key, State instance is value
+	private State _currentState; 
+
+	public override void _Ready(Node parent)
+	{
+		// Gather all states and save them
+		_states = new Dictionary<string, State>();
+		foreach (Node node in GetChildren()) 
+		{
+			if (node is State state)
+			{
+				_states[node.Name] = state;
+				state.fsm = this;
+				state.Ready();
+				state.parent = parent; 
+				state.Exit(); 
+				GD.Print(node.Name);
+			}
+		}	
+
+		// Enter intial state
+		_currentState = GetNode<State>(intialState);
+		_currentState.Enter();
+	}
+
+	// ----- Proccessors and Update Methods -----
+	public override void _Process(double delta)
+	{
+		_currentState.Update((float) delta);
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		_currentState.PhysicsUpdate((float) delta);
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		_currentState.HandleInput(@event);
+	}
+
+	public void StateTransition(string key)
+	{
+		// States exists + Not all ready state
+		if (!_states.ContainsKey(key) || _currentState == _states[key])
+		{
+			return;
+		}
+
+		// Exit current state and go into the next
+		_currentState.Exit();
+		_currentState = _states[key];
+		_currentState.Enter();
+	}
+}
